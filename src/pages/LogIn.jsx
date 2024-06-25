1dcaa70e4887b5caae13c2bb4e02512c42cb6461
AndreaGul <andreagulino99@gmail.com>
2024-06-25 16:01:07 +0200

    feat:prima parte del login

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,20 +1,53 @@
+import { useState } from "react";
 import { useAuth } from "../contexts/AuthContext"
 
 export default function () {
 
     const { login } = useAuth();
 
-    const handleLogin = e =>{
+    const initialData = {
+        email: '',
+        password: ''
+    }
+
+    const [formData, setFormData] = useState(initialData);
+
+    const [loginError, setLoginError]= useState(null);
+
+    const changeData = (key, value)=>{
+        setFormData(curr=>({
+            ...curr,
+            [key]:value
+        }));
+    }
+
+    const handleLogin = async e =>{
         e.preventDefault();
-        login();
+        try{
+            await login(formData)
+            setFormData(initialData)
+        }catch(err){
+            setLoginError(err)
+        }
+        
     }
 
 
     return(
         <>
         <form onSubmit={handleLogin}>
-            <input type="text" placeholder="email" />
-            <input type="password" placeholder="password" />
+            <input 
+            type="text" 
+            placeholder="email" 
+            value={formData.email} 
+            onChange={e=>changeData('email', e.target.value)} />
+
+            <input 
+            type="password" 
+            placeholder="password"
+            value={formData.password}
+            onChange={e=>changeData('password', e.target.value)} />
+
             <button>Accedi</button>
         </form>
         </>
